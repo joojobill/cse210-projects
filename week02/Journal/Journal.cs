4865018fad8e0d66ebc95cad96a6607bf85cc417
joojobill <hoo21001@byui.edu>
2025-01-17 15:50:04 +0000

    nice

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -31,7 +31,16 @@ public class Journal
 
     public void SaveToFile(string file)
     {
-        
+        using (StreamWriter writer = new StreamWriter (file))
+        {
+            foreach (var entry in _entries)
+            {
+                Console.WriteLine($"{entry._date} {entry._promptText} {entry._entryText}");
+            }
+        }
+        Console.WriteLine($"Journal save to {file}");
+
+
 
     }
     public void LoadFromFile(string file)
