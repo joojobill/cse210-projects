391e1bcdbd10515854887fa326fdaaec9767fda7
joojobill <hoo21001@byui.edu>
2025-02-05 18:33:58 +0000

    second

--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -2,5 +2,32 @@ using System;
 
 public class Customer
 {
-    
+     // Private member variables
+    private string _name;
+    private Address _address;
+
+    // Constructor
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    // Method to check if the customer lives in the USA
+    public bool LivesInUSA()
+    {
+        return _address.IsInUSA();
+    }
+
+    // Getter for the customer's name
+    public string GetName()
+    {
+        return _name;
+    }
+
+    // Getter for the customer's address
+    public Address GetAddress()
+    {
+        return _address;
+    }
 }
--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -2,5 +2,36 @@ using System;
 
 public class Product 
 {
-    
+    // Private member variables
+    private string _name;
+    private string _productId;
+    private double _price;
+    private int _quantity;
+
+    // Constructor
+    public Product(string name, string productId, double price, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    // Method to calculate the total cost of the product
+    public double GetTotalCost()
+    {
+        return _price * _quantity;
+    }
+
+    // Getter for the product name
+    public string GetName()
+    {
+        return _name;
+    }
+
+    // Getter for the product ID
+    public string GetProductId()
+    {
+        return _productId;
+    }
 }
