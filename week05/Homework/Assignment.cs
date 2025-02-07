55b403b23b35825ef241963aaf4a1ecf6d04e3c6
joojobill <hoo21001@byui.edu>
2025-02-07 14:21:49 +0000

    done

--- week05/Homework/Assignment.cs
+++ week05/Homework/Assignment.cs
@@ -6,6 +6,12 @@ public class Assignment
     private string _studentName = "";
     private string _topic = "";
 
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
     public string GetStudentName()
     {
         return _studentName;
